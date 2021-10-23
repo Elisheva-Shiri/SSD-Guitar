d84eaa125439f949526b52ac57c7022f6dd59c86
Elisheva Shiri Decktor <laal.esd@gmail.com>
2021-10-23 22:27:56 +0300

    Addopted code to Teensy

--- sturm.ino
+++ sturm.ino
@@ -1,9 +1,16 @@
 // include the Servo library
 #include <Servo.h>
 
-const int COMMON  = 2;
-const int B_PLUS  = 10;
-const int B_MINUS = 11;
+const int COMMON = 2;
+
+struct StateButton
+{
+  bool state;
+  int a;
+  int b;
+};
+
+StateButton Buttons[3];
 
 const int SERVO_NUM = 6 ;
 const int STRUM_NUM = 3 ;
@@ -12,67 +19,76 @@ unsigned long lastFire = 0;
 unsigned long currStrum = 0;
 
 // create a servo objects
-Servo myServos[SERVO_NUM] ={};  
+Servo myServos[SERVO_NUM] ={};
+bool myServosState[SERVO_NUM] ={};  
 
-const int servoPin[SERVO_NUM] = {4,5,6,7,8,9}; // digital pin used to connect the servo
+const int servoPin[SERVO_NUM] = {6,7,8,9,10,11}; // digital pin used to connect the servo
 int angles[SERVO_NUM] = {0};   // variable to hold the angle for the servo motor
-int angleOpen = 70; //middle 85
-int angleClose = 100 ;
+int angleOpen = 85;
+int angleClose = 115;
 
 void configureCommon() {
-  pinMode(COMMON, INPUT_PULLUP);
+ // pinMode(COMMON, INPUT_PULLUP);
 
-  pinMode(B_PLUS, OUTPUT);
-  digitalWrite(B_PLUS, LOW);
+ // pinMode(B_PLUS, OUTPUT);
+ // digitalWrite(B_PLUS, LOW);
 
-  pinMode(B_MINUS, OUTPUT);
-  digitalWrite(B_MINUS, LOW);
+ // pinMode(B_MINUS, OUTPUT);
+  //digitalWrite(B_MINUS, LOW);
 }
 
+bool getButtonState(StateButton sb)
+{
+  Serial.print(digitalRead(Buttons[0].a));
+  //Serial.println(digitalRead(sb.b));
+  return digitalRead(sb.a);
+}
 void configureDistinct() {
   pinMode(COMMON, OUTPUT);
   digitalWrite(COMMON, LOW);
 
-  pinMode(B_PLUS, INPUT_PULLUP);
-  pinMode(B_MINUS, INPUT_PULLUP);
+ // pinMode(B_PLUS, INPUT_PULLUP);
+ // pinMode(B_MINUS, INPUT_PULLUP);
 }
 
 void pressInterrupt() { // ISR
-  if (millis() - lastFire < 200) { // Debounce
-    return;
-  }
-  lastFire = millis();
-
-  configureDistinct(); // Setup pins for testing individual buttons
-
-  if (digitalRead(B_PLUS) == LOW) {
-    currStrum++;
-    currStrum %= STRUM_NUM;
-  } else if (digitalRead(B_MINUS) == LOW){
-    if (currStrum == 0)
-    {
-      currStrum = STRUM_NUM - 1;
-    }
-    else
-    {
-      currStrum--;
-    }
-  }
-  Serial.println(currStrum);
-
-  configureCommon(); // Return to original state
 }
 
 void setup() {
   Serial.begin(9600); // open a serial connection to your computer
+  
+  Buttons[0].a = 14;
+  Buttons[0].b = 15;
+  digitalWrite(Buttons[0].a, LOW);
+  digitalWrite(Buttons[0].b, LOW);
+  pinMode(Buttons[0].a, INPUT);
+  pinMode(Buttons[0].a, INPUT);
+  Buttons[1].state = getButtonState(Buttons[0]);
+  
+  Buttons[1].a = 18;
+  Buttons[1].b = 19;
 
+  pinMode(Buttons[1].a, INPUT);
+  pinMode(Buttons[1].a, INPUT);
+  Buttons[1].state = getButtonState(Buttons[1]);
+  
+  Buttons[2].a = 22;
+  Buttons[2].b = 23;
+  digitalWrite(Buttons[2].a, LOW);
+  digitalWrite(Buttons[2].b, LOW);
+  pinMode(Buttons[2].a, INPUT);
+  pinMode(Buttons[2].a, INPUT);
+  Buttons[1].state = getButtonState(Buttons[2]);
+  
   setAngles();
-  configureCommon();
-  attachInterrupt(digitalPinToInterrupt(COMMON), pressInterrupt, FALLING);
+  //configureCommon();
+  //attachInterrupt(digitalPinToInterrupt(COMMON), pressInterrupt, FALLING);
   
   for (int i = 0; i < SERVO_NUM; i++){
     myServos[i].attach(servoPin[i]); // attaches the servo pin to the servo object
   }
+
+  Serial.write("done");
 }
 
 void loop() {
@@ -92,19 +108,46 @@ void loop() {
   }
 }
 
+void checkState()
+{
+  if (Buttons[0].state != getButtonState(Buttons[0]))
+  {
+    
+  }
+
+  if (Buttons[1].state != getButtonState(Buttons[1]))
+  {
+    currStrum++;
+    currStrum %= STRUM_NUM;
+  }
+
+  if (Buttons[2].state != getButtonState(Buttons[2]))
+  {
+    if (currStrum == 0)
+    {
+      currStrum = STRUM_NUM - 1;
+    }
+    else
+    {
+      currStrum--;
+    }
+  }
+}
 
 void setAngles (){
   for (int i = 0; i < SERVO_NUM; i++){
+    myServosState[i] = true;
     myServos[i].write(angleOpen);     // set the servo position
   }
 }
 
-int getAngle(int angle) {
-  if (angle == angleOpen)
+int getAngle(int i) {
+  if (myServosState[i])
   {
+    myServosState[i] = false;
     return angleClose;
   }
-
+  myServosState[i] = true;
   return angleOpen;
 }
 
@@ -115,7 +158,8 @@ int getBase() //will be controlled by a button
 
 void playIndex(int i, bool doDelay = true)
 {
-  myServos[i].write(getAngle(myServos[i].read()));// set the servo position
+  checkState();
+  myServos[i].write(getAngle(i));// set the servo position
   if (doDelay)
     delay(300);
 }
